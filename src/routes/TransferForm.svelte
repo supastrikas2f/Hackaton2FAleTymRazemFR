47d1f6553799f32949e7ad2a79f27f0adbf8cf8f
Bejg <bejq07@gmail.com>
2024-04-21 20:17:05 +0200

    chuj

---
+++ src/routes/TransferForm.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  export let players;
+
+  const dispatch = createEventDispatcher();
+  let receiver;
+  let amount;
+
+  const handleTransfer = () => {
+    dispatch('transfer', {receiver, amount });
+    receiver = '';
+    amount = '';
+  };
+</script>
+
+<div>
+  <h2>Przelew</h2>
+
+  <select bind:value={receiver}>
+    {#each players as player}
+      <option value={player.name}>{player.name}</option>
+    {/each}
+  </select>
+
+  <input type="number" bind:value={amount} placeholder="Kwota" />
+
+  <button on:click={handleTransfer}>Wykonaj przelew</button>
+</div>
